ce184c313c6db39ff93c5e41279f4855b986478c
Victor Soldera <victorsolderalf@gmail.com>
2021-11-15 12:32:40 -0300

    Adding NULL to linked list

---
+++ VirtualMachine/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+D
+ViewController.swift,9/3/930c057b7e038ff8b325204f31cf5666f008f30e
--- VirtualMachine/MachineCodeInterpreter.swift
+++ VirtualMachine/MachineCodeInterpreter.swift
@@ -13,19 +13,37 @@ class MachineCodeInterpreter {
     init(fileContent: String) {
         self.fileContent = fileContent
     }
+    //            if let range = chars.range(of: "JMP ") {
+//                let jmp = chars[range.upperBound...]
+//                print("JMP: ", jmp)
+//            }
     
     func machine(fileContent: String) {
-        let string = fileContent.components(separatedBy: .newlines)
         
-        for chars in string {
+        let string = fileContent.components(separatedBy: .newlines)
+        //print(string)
+        var partial: String
+        var i = 0
+        let result: String = string.reduce(into: "") { partial, toTrim in
+            guard
+                    let range = toTrim.range(of:"\t"),
+                    range.upperBound < toTrim.endIndex
+                    else { return }
             
-            if chars == "\t" {
-                
-                print()
-                
+            
+            partial.append("\(toTrim[range.upperBound..<toTrim.endIndex]) ")
+            //print("\(toTrim[range.upperBound..<toTrim.endIndex]) ")
+            
+            if range.lowerBound > toTrim.startIndex  {
+                //Pega valor antes do NULL
+                line.append(code(linha: i, inst: "NULL", atrib1: "\(toTrim[toTrim.startIndex..<range.lowerBound])"))
             }
             
+            i+=1
         }
+        
+        print(line)
+        
+        //print(result)
     }
-    
 }
